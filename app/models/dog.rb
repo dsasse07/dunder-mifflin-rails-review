76c15f7646d928428a8debe62a4bbfd29be7d6c2
Daniel Sasse <dsasse07@gmail.com>
2021-01-16 14:16:25 -0500

    validations done

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,5 @@
 class Dog < ApplicationRecord
   has_many :owners, class_name: "Employee", foreign_key: :dog_id
+  validates :name, :breed, presence: true
+  validates :age, presence: true, numericality: {greater_than: 0}
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,17 @@
 class Employee < ApplicationRecord
   belongs_to :dog
+  validates :first_name, :last_name, presence: true
+  validates :title, presence: true, uniqueness: true
+  validates :dog_id, presence: true
+  validates :office, presence: true
+  validate :https_url
+  validates :alias, presence: true, uniqueness: true
+
+  def https_url
+    unless !!self.img_url.match(/\bhttps:\/\/.*/) 
+      self.errors.add(:img_url, "Image url must start with https://")
+    end
+  end
+
+
 end
